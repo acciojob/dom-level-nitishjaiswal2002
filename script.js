c1805c953c20d0204fb8b794d3721d90dd66e56d
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-22 10:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded" , () => {
+	let levelElement = document.getElementByid("level");
+	console.log(levelElement);
+	let level= 1;
+	while (true) {
+		if(levelElement.tagName ==="HTML"){
+			break;
+		}
+		console.log(levelElement);
+		levelElement = levelElement.parentNode;
+		level++;
+	}
+
+	alert('The level of the element is: ${level}');
+});
